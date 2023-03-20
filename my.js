f7e7760536a8b208529c8b07ba29e7f2b9c7104d
Artist <krlmnhzn@gmail.com>
2023-03-20 19:24:59 +0200

    New lesson after page with sea

--- my.js
+++ my.js
@@ -1,1 +1,16 @@
-console.log(135);
+const factorial = (n) => {
+    if (n === 0) {
+        return 1;
+    }
+
+    const iter = (counter, acc) => {
+        if (counter === 1) {
+            return acc;
+        }
+        return iter(counter - 1, counter * acc);
+    };
+
+    return iter(n, 1);
+};
+
+
