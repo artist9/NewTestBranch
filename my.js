77225cef1191d8acc993752d0ec4d9da55864d58
Artist <krlmnhzn@gmail.com>
2023-03-20 20:28:47 +0200

    Delete four item menu

--- my.js
+++ my.js
@@ -1,16 +1,3 @@
-const factorial = (n) => {
-    if (n === 0) {
-        return 1;
-    }
-
-    const iter = (counter, acc) => {
-        if (counter === 1) {
-            return acc;
-        }
-        return iter(counter - 1, counter * acc);
-    };
-
-    return iter(n, 1);
-};
+console.log(5);
 
 
