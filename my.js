f7e2ad52eda2288e10f1ee2c86814976eda81deb
Artist <krlmnhzn@gmail.com>
2023-04-21 20:01:59 +0300

    Setting VScode

--- my.js
+++ my.js
@@ -1,4 +1,14 @@
-console.log(5);
-console.log("Hello!");
+function* genatesequence() {
+	yield 1;
+	yield 2;
+	return 3;
+}
+
+let generator = generateSequence();
+
+let one = generator.next();
+
+console.log(one); // {value: 1, done: false}
+
 
 
